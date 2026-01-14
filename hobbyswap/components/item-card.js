d608d0b6063c0ae537849610955d321ab63ceaa4
amtchua <amtchua@github.com>
2026-01-13 23:32:32 -0500

    Created base item-card component

---
+++ hobbyswap/components/item-card.js
@@ -0,0 +1,33 @@
+import Image from "next/image";
+import { Button } from "react-bootstrap";
+import BookmarkIcon from "./bookmark-icon";
+
+export default function ItemCard({ img, name, desc, saved }) {
+  return (
+    <div style={{ width: 280 }}>
+      <Image
+        className="drop-shadow"
+        alt={name}
+        src={img}
+        width={278}
+        height={385}
+      />
+      <div>
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "space-between",
+            marginTop: 20,
+          }}
+        >
+          <h3>{name}</h3>
+          <BookmarkIcon fill={saved} />
+        </div>
+        <p>{desc}</p>
+      </div>
+      <Button style={{ width: "100%" }} variant="secondary">
+        Trade Now
+      </Button>
+    </div>
+  );
+}
