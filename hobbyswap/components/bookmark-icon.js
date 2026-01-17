918e2b102188e7c5784b1b9adce15875945191bc
amtchua <amtchua@github.com>
2026-01-16 19:25:40 -0500

    fixed bookmark icon a bit

--- hobbyswap/components/bookmark-icon.js
+++ hobbyswap/components/bookmark-icon.js
@@ -1,24 +1,14 @@
-export default function BookmarkIcon({ fill, size = 32, ...props }) {
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBookmark as solidBookmark } from "@fortawesome/free-solid-svg-icons";
+import { faBookmark as outlineBookmark } from "@fortawesome/free-regular-svg-icons";
+
+export default function BookmarkIcon({ fill, size = "xl", ...props }) {
   return (
-    <div {...props}>
-      <svg
-        width={size}
-        height={size}
-        viewBox="0 0 32 32"
-        fill="none"
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        <g clip-path="url(#clip0_473_375)">
-          <path
-            d="M25 29L16 22.8889L7 29V9.44444C7 8.79614 7.27092 8.17438 7.75315 7.71596C8.23539 7.25754 8.88944 7 9.57143 7H22.4286C23.1106 7 23.7646 7.25754 24.2468 7.71596C24.7291 8.17438 25 8.79614 25 9.44444V29Z"
-            fill={fill ? "#001F54" : ""}
-            stroke="#001F54"
-            strokeWidth="3"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-          />
-        </g>
-      </svg>
-    </div>
+    <FontAwesomeIcon
+      color="#001F54"
+      size={size}
+      icon={fill ? solidBookmark : outlineBookmark}
+      {...props}
+    />
   );
 }
