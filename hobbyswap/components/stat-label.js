324d4ad8f0045c8dd0f5cf4684dc2ae9f058f57c
amtchua <amtchua@github.com>
2026-01-16 19:39:08 -0500

    edited stat label component and added caption styling

--- hobbyswap/components/stat-label.js
+++ hobbyswap/components/stat-label.js
@@ -1,16 +1,7 @@
 export default function StatLabel({ label, stat, ...props }) {
   return (
     <div {...props}>
-      <p
-        style={{
-          fontSize: 12,
-          color: "#777070",
-          marginBottom: 2,
-          textTransform: "uppercase",
-        }}
-      >
-        {label}
-      </p>
+      <p className="text-uppercase mb-1 caption">{label}</p>
       <strong className="h4">{stat}</strong>
     </div>
   );
