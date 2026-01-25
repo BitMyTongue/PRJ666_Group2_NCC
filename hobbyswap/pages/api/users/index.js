d20418f3ac1dd2a37d59fade017def8bd65e8a79
Tessa <new@news-mbp-9.home>
2026-01-25 16:08:59 -0500

    Add hashing and salting password

--- hobbyswap/pages/api/users/index.js
+++ hobbyswap/pages/api/users/index.js
@@ -9,25 +9,37 @@ export default async function handler(req, res) {
 
     switch (method) {
       case "GET":
-        let users = await UserModel.find().exec();
-        res.status(200).json({ users });
-        break;
+      const users = await UserModel.find().select("-password");
+      return res.status(200).json({ users });
+      break;
       case "POST":
         // Check if user already exists in db
-        const existingUser = await UserModel.findOne({ $or: [{email}, {username}]});
-        if (existingUser){
-          return res.status(409).json({ error: "Email or username already exists"});
-        }
+         if (!firstName || !lastName || !username || !email || !password) {
+        return res.status(400).json({ error: "Missing fields" });
+      }
 
-        let newUser = new UserModel({ firstName, lastName, username, email, password });
-        await newUser.save();
+      const existingUser = await UserModel.findOne({ $or: [{email}, {username}]});
+      if (existingUser){
+          return res.status(409).json({ error: "Email or username already exists"});
+      }
+
+      const user = await UserModel.create({
+        firstName,
+        lastName,
+        username,
+        email,
+        password,
+      });
+      const userObj = user.toObject();
+      delete userObj.password;
         res.status(201).json({ message: "User Created", user: newUser });
-        break;
-      default:
-        res.setHeader("Allow", ["GET", "POST"]);
-        res.status(405).end(`Method ${method} Not Allowed`);
-    }
+    break;
+    default:
+    res.setHeader("Allow", ["GET", "POST"]);
+    res.status(405).end(`Method ${method} Not Allowed`);
+  }
   } catch (err) {
+    console.error("API USERS ERROR:", err);
     res.status(500).json({ error: err.message });
   }
 }
